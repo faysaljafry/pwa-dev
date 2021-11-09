c03bcbf097b2970debbe46e977a12b8598116b5f
faysaljafry <faysaljafryofficial@gmail.com>
2021-11-09 21:36:33 +0500

    deploy

---
+++ precache-manifest.4a493d54c29fd5669a6d1fbfe4cd3b55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0c435d9f7cc7d71607",
+    "url": "css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "69475bbe5039a59b60d34047415af6f7",
+    "url": "index.html"
+  },
+  {
+    "revision": "1f0c435d9f7cc7d71607",
+    "url": "js/app.cf1021b7.js"
+  },
+  {
+    "revision": "df711e2a592582acac55",
+    "url": "js/chunk-vendors.457aa08e.js"
+  },
+  {
+    "revision": "48281ead8e3117879c222605840dce8a",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
